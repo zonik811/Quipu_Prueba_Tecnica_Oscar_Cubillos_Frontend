26d7d43f23bd39a8e9ca1230bbd8d7e2d2789dc6
zonik811 <oscarcubillos811@gmail.com>
2026-08-01 19:00:55 -0500

    fix: simplificar regex de getCookie para CSRF token

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,4 +1,4 @@
 export function getCookie(name) {
-  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'))
-  return match ? decodeURIComponent(match[2]) : null
+  const match = document.cookie.match(new RegExp(`${name}=([^;]+)`))
+  return match ? decodeURIComponent(match[1]) : null
 }
